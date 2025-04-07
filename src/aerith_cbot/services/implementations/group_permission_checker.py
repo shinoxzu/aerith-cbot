0bc765472942648a1bdbdcca7b3035c7283f8839
tenkawaa <ffl1pyyy@gmail.com>
2025-04-07 13:58:29 +0300

    check status of member instead of checking all members

--- src/aerith_cbot/services/implementations/group_permission_checker.py
+++ src/aerith_cbot/services/implementations/group_permission_checker.py
@@ -1,6 +1,7 @@
 import logging
 
 from aiogram import Bot, exceptions
+from aiogram.enums import ChatMemberStatus
 
 from aerith_cbot.services.abstractions import PermissionChecker
 
@@ -12,12 +13,14 @@ class GroupPermissionChecker(PermissionChecker):
 
     async def check_permissions(self, chat_id: int, user_id: int) -> bool:
         try:
-            chat_admins = await self._bot.get_chat_administrators(chat_id)
+            chat_member = await self._bot.get_chat_member(chat_id, user_id)
         except exceptions.TelegramAPIError as err:
             self._logger.error("Cannot get chat %s admins cause of %s", chat_id, err, exc_info=err)
             return False
 
-        for chat_admin in chat_admins:
-            if chat_admin.user.id == user_id:
-                return True
+        if (
+            chat_member.status == ChatMemberStatus.ADMINISTRATOR
+            or chat_member.status == ChatMemberStatus.CREATOR
+        ):
+            return True
         return False
